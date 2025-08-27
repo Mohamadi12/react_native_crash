43c50b9dc8b8eb022b730b7fdda460799383ccff
Nana Mohamadi <adilnana78@gmail.com>
2025-08-27 09:04:53 +0000

    Routage basé sur des fichiers

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function Index() {
+const Home = () => {
   return (
-    <View>
-      <Text>Welcome to the app!</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home Page</Text>
+      <Link href="/about">About</Link>
+      <Link href="/profile">Profile</Link>
     </View>
   );
-}
+};
+
+export default Home;
